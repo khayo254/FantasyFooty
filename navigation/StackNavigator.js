8ffa08b9f834d5003186b85dc56c7098e0f85bef
khayo254 <khayo1998@gmail.com>
2024-08-06 21:45:29 +0300

    fanasy footy

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createStackNavigator();
+
+function StackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default StackNavigator;
---
+++ npm_dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm install @react-navigation/native
+npm install @react-navigation/stack
+npm install react-native-gesture-handler
+npm install react-native-reanimated
+npm install react-native-screens
+npm install react-native-safe-area-context
+npm install @react-native-community/masked-view
